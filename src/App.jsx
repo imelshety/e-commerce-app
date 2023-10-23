f41e4236012016c2e0fa96dfec9a7feb395c9e1b
imelshety <mohamedkingman99@gmail.com>
2023-10-23 20:09:22 +0300

    add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Home from "./components/Home"
 import Navbar from "./components/Navbar"
 
 function App() {
@@ -5,9 +6,7 @@ function App() {
 
     <>
       <Navbar />
-      <h1 className="text-3xl font-bold underline text-mainYellow">
-        Hello world!
-      </h1>
+      <Home />
     </>
   )
 }
